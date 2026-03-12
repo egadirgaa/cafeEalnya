36ab45a690b0e5475cb2fc5d1a681e0b60e0e000
egdrga <116239318+egadirgaa@users.noreply.github.com>
2026-03-12 15:33:50 +0700

    Add 'role' field to user creation in authController

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -47,6 +47,7 @@ class AuthController extends Controller
             'name'     => $data['name'],
             'email'    => $data['email'],
             'password' => Hash::make($data['password']),
+            'role' => 'admin'
         ]);
 
         Auth::login($user);
