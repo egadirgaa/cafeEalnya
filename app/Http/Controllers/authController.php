f8098fee6387996da658428e8b27c51a62a40350
egdrga <116239318+egadirgaa@users.noreply.github.com>
2026-03-12 15:38:47 +0700

    Remove hardcoded 'admin' role from user creation
    Removed hardcoded role assignment for user creation.

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -47,7 +47,6 @@ class AuthController extends Controller
             'name'     => $data['name'],
             'email'    => $data['email'],
             'password' => Hash::make($data['password']),
-            'role' => 'admin'
         ]);
 
         Auth::login($user);
